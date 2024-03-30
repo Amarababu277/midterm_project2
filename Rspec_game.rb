80d26d977e3d08c4d45213fed5487704ac7de245
Amarababu277 <158104913+amarababu277@users.noreply.github.com>
2024-03-29 23:13:55 -0500

    Rspec_card - player

---
+++ Rspec_game.rb
@@ -0,0 +1,13 @@
+require 'game.rb'
+
+
+RSpec.describe Game do
+  describe "#initialize" do
+    it "creates a new game with players" do
+      game = Game.new(4)
+      expect(game.players.size).to eq(4)
+    end
+  end
+
+  # Add more test cases as needed
+end
